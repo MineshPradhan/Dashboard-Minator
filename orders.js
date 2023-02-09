2a37bd6b8d20e7e1617e21e2aa9a1a3778ee349a
MineshPradhan <mineshpradhan2002@gmail.com>
2023-02-09 23:14:25 +0530

    orders data added

---
+++ orders.js
@@ -0,0 +1,56 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Kimune Aura Drone',
+        productNumber: '58725',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Micro Beetle Drone',
+        productNumber: '75486',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Kimune Aura Drone',
+        productNumber: '58725',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Micro Beetle Drone',
+        productNumber: '75486',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
